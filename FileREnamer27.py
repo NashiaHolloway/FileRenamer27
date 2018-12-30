9a7b1f7a87d478e435a4394a3e1980b2586169f4
unknown <nashiaholloway1@gmail.com>
2018-12-29 21:11:22 -0500

    Better, faster, stronger

--- FileREnamer27.py
+++ FileREnamer27.py
@@ -4,30 +4,45 @@ import os
 
 
 def main():
-    path_exists()
+    file_exists()
 
 
 # Check that file path exists
-def path_exists():
+def file_exists():
     while True:
         # Ask user to enter the filepath of the files they want to rename
-        filePath = raw_input("Enter directory of files you want to rename: ")
-        if os.path.isdir(filePath):
-            print("File path found...")
-            print("")
-            # Ask user to enter what text they want to pre-pend to the files in said path
-            text = raw_input("Enter text to pre-pend: ")
+        file_path = raw_input("Enter directory of files you want to rename (q to quit): ").lower()
+        if file_path != "q":
+            if os.path.isdir(file_path):
+                print("File path found...")
+                print("")
+                renamer(file_path)
+            else:
+                print("Invalid path; try again")
+                continue  # restart loop
+        else:
+            break
+
+
+def renamer(file_path):
+    while True:
+        # Ask user to enter what text they want to pre-pend to the files in said path
+        text = raw_input("Enter text to pre-pend: ")
+        # Make sure user input is what they want
+        ans = raw_input("Are you sure you want to pre-pend '" + text.upper() + "'? (y/n) ").lower()
+        if ans == "y":
             # Loop through files and pre-pend user's text to the files
-            for filename in os.listdir(filePath):
+            for filename in os.listdir(file_path):
                 # pre-pend user text
                 new_filename = text + filename
                 print(new_filename)
-                os.rename(os.path.join(filePath, filename), os.path.join(filePath, new_filename))
+                os.rename(os.path.join(file_path, filename), os.path.join(file_path, new_filename))
             print("Finished")
             break
         else:
-            print("Invalid path; try again")
-            continue  # restart loop
+            print("")
+            renamer(file_path)
+            break
 
 
 main()
